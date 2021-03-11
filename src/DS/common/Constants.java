2edaf30fdb38b2167c7b2c870599bca7b3860c44
kguniverse <kkwang@outlook.com>
2021-03-11 14:32:25 +0800

    change constants upperbound

--- src/DS/common/Constants.java
+++ src/DS/common/Constants.java
@@ -1,7 +1,7 @@
 package DS.common;
 
 public class Constants {
-    public final static int MAXIMUM_Edge  = 405;
+    public final static int MAXIMUM_Edge  = 4000005;
     public final static int MAXIMUM_Node  = 405;
     public static int NowEdgeNum = 0;
     public static int NowNodeNum = 0;
